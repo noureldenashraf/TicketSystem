4fd75b050d1cda429ba135b29d6d4faa8f79696f
Eng-Nour <n.ashraf956@gmail.com>
2025-09-24 02:45:01 +0300

    made sure to block dashboard for normal users

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -23,10 +23,12 @@
     @if (Route::has('login'))
         <nav class="flex items-center gap-4">
             @auth
+                @if(auth()->user()->role == "admin")
                 <a href="{{ url('/dashboard') }}"
                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                     Dashboard
                 </a>
+                @endif
             @else
                 <a href="{{ route('login') }}"
                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
@@ -9,7 +10,7 @@ Route::get('/', function () {
     return redirect()->route("ticket.index");
 });
 Route::patch("/ticket/{ticket_id}",[TicketController::class,"toggle"])->middleware("auth","verified")->name("ticket.toggle");
-Route::get('/dashboard',[\App\Http\Controllers\DashboardController::class , "index"])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard',[DashboardController::class , "index"])->middleware(['auth', 'verified',"admin"])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
